89c290aab08525d2179830e0981f4188ed4e15f0
Tonys <info@tony-schiller.com>
2024-01-28 22:01:29 +0100

    Update assetsInclude patterns in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
   ],
@@ -13,5 +14,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  assetsInclude: ['@/*.svg', '**/*.webp'],
+  assetsInclude: ['**/*.svg', '**/*.webp'],
 })
