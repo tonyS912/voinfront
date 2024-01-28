282a8be84bfd30d6fe326b6995e73759256e49b3
Tony <66740164+tonyS912@users.noreply.github.com>
2024-01-28 21:26:18 +0100

    Update MainSidebar.vue

--- src/components/MainSidebar.vue
+++ src/components/MainSidebar.vue
@@ -8,10 +8,10 @@
                 <router-link
                     to="/summary"
                     class="btn btn-light py-3 d-flex justify-items-start list-group-item border-0"
-                    ><img class="me-2" src="./../assets/icons/menu_summary.svg" />Summary</router-link
+                    ><img class="me-2" src="./assets/menu_summary.svg" />Summary</router-link
                 >
                 <li class="btn btn-light py-3 d-flex justify-items-start list-group-item border-0">
-                    <img class="me-2" src="./../assets/icons/menu_board.svg" />Board
+                    <img class="me-2" src="./assets/menu_board.svg" />Board
                 </li>
                 <li class="btn btn-light py-3 d-flex justify-items-start list-group-item border-0">
                     <img class="me-2" src="./../assets/icons/menu_add_task.svg" />Add Task
